feec73880fdde5dd7b0612eb75b65e08ba847d33
Nathan Qian <ntqian@email.wm.edu>
2020-08-23 16:02:55 -0400

    ad hoc work on verifying the engine

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, Binary, Date, ForeignKey, create_engine
+from sqlalchemy import Column, Integer, String, Binary, Date, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -17,15 +17,13 @@ class Player(Base):
     name = Column(String, nullable=False)
     race = Column(Integer)
     matchups = Column(Binary)
-    children = relationship("Game")
 
 
 class Map(Base):
     __tablename__ = 'map'
 
-    id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    children = relationship("Game")
 
 
 class Game(Base):
@@ -34,8 +32,10 @@ class Game(Base):
     id = Column(Integer, primary_key=True)
     date = Column(Date)
     map = Column(Integer, ForeignKey('map.id'))
-    winner = Column(Integer, ForeignKey('player.id'))
-    loser = Column(Integer, ForeignKey('player.id'))
+    winner_id = Column(Integer, ForeignKey('player.id'))
+    winner = relationship('Player', foreign_keys=[winner_id])
+    loser_id = Column(Integer, ForeignKey('player.id'))
+    loser = relationship('Player', foreign_keys=[loser_id])
     winner_race = Column(Integer, nullable=False)
     loser_race = Column(Integer, nullable=False)
 
